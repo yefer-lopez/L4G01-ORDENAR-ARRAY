bd0b56df5a2b6d8f32855160dcdd089ea48c3d51
Yefer Lopez <yeferlopez09@gmail.com>
2020-02-25 20:50:52 -0500

    modificando codigo

--- ordenarray.php
+++ ordenarray.php
@@ -1,11 +1,41 @@
-<?php
-array=[50,20,200,30,1,2]
-?>
 <!DOCTYPE html>
-<html lang="es">
-<head> 
-<meta charset= UFF-8>
+<head>
+<h2>METODO DE ORDENAMIENTO</h2>
 </head>
 <body>
+<h3>METODO BURBUJA</h3>
 </body>
 </html>
+
+<?php
+//ORDENAMIENTO
+function burbuja($array)
+{
+    for($i=1;$i<count($array);$i++)
+    {
+        for($j=0;$j<count($array)-$i;$j++)
+        {
+            if($array[$j]>$array[$j+1])
+            {
+                $k=$array[$j+1];
+                $array[$j+1]=$array[$j];
+                $array[$j]=$k;
+            }
+        }
+    }
+ 
+    return $array;
+}
+ 
+$ingreso=array(55,9,4,7,3,8,2,1,6);
+ 
+echo "VALORES DESORDENADOS<br>";
+for($i=0;$i<count($ingreso);$i++)
+    echo $ingreso[$i]."\n";
+ 
+$salida=burbuja($ingreso);
+ 
+echo "<br><br>VALORES ORDENADOS<br>";
+for($i=0;$i<count($salida);$i++)
+    echo $salida[$i]."\n";
+?>
